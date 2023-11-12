310c1ebd1af98141a811d33574a645eaf756a7da
yakuran11 <rittitarou@yahoo.co.jp>
2023-11-12 10:09:02 +0900

    🗨️comment: applicationレイヤーのコメント整理

--- lib/app/application/usecase/purchase/state/child_view_purchase_provider.dart
+++ lib/app/application/usecase/purchase/state/child_view_purchase_provider.dart
@@ -6,6 +6,7 @@ import '../purchase_usecase.dart';
 part 'child_view_purchase_provider.g.dart';
 
 /// (子供参照用)購入情報
+/// データの参照頻度を減らすため、`keepAlive`を指定
 @riverpod
 Stream<Purchase?> childViewPurchase(
   ChildViewPurchaseRef ref, {
