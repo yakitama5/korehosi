dbfef755ca34f2afb48bb8a971c4de8c71d2b218
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2023-11-13 09:50:09 +0900

    🐛fix: `childViewPurchase`Providerの制約エラーを解消

--- lib/app/application/usecase/purchase/state/child_view_purchase_provider.dart
+++ lib/app/application/usecase/purchase/state/child_view_purchase_provider.dart
@@ -14,6 +14,6 @@ Stream<Purchase?> childViewPurchase(
   required String itemId,
 }) {
   return ref
-      .watch(purchaseUsecaseProvider)
+      .read(purchaseUsecaseProvider)
       .fetchByItemIdForChild(groupId: groupId, itemId: itemId);
 }
