24c3ba01fac194a97efbc30bdf12061961748eb4
yakitama5 <rittitarou@yahoo.co.jp>
2023-11-14 11:29:55 +0900

    🐛fix: iOSの認証エラーを解消

--- lib/app/infrastructure/google/state/google_sign_in.dart
+++ lib/app/infrastructure/google/state/google_sign_in.dart
@@ -1,12 +1,9 @@
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../config/google_sing_in_config.dart';
-
 part 'google_sign_in.g.dart';
 
 /// Google Sign In
 /// テスト時にDIすることを考慮して、Providerとして定義
 @riverpod
-GoogleSignIn googleSignIn(GoogleSignInRef ref) =>
-    GoogleSignIn(clientId: googleSigninClientId);
+GoogleSignIn googleSignIn(GoogleSignInRef ref) => GoogleSignIn();
