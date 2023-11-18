96c8e8567478a5f1f7b133980f0eb81897b0296e
yakuran11 <rittitarou@yahoo.co.jp>
2023-11-18 14:09:57 +0900

    🚧wip: ふりかえりの詳細ページのデザインを作成中

---
+++ lib/app/presentation/pages/analyze/analyze_detail_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class AnalyzeDetailPage extends HookConsumerWidget {
+  const AnalyzeDetailPage({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: PageView(),
+    );
+  }
+}
