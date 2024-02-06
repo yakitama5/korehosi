3dbd823ec05e290cf169c13e0f3cd727ed284d51
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-02-06 16:43:32 +0900

    ✨feat: 欲しい物追加時の通知処理を作成

--- lib/app/infrastructure/local_notifications/state/android_notification_channel.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-// TODO(yakitama5): 説明分などを適正化すること
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  'high_importance_channel',
-  'High Importance Notifications',
-  description: 'This channel is used for important notifications.',
-  importance: Importance.max,
-);
