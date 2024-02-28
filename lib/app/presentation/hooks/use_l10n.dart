195f6491ab9b27d9ebbb01dfd11bc1a2a4d15214
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-02-28 18:16:50 +0900

    ♻️refactor: 欲しい物の明細画面郡を横断して利用する状態管理をリファクタリング

---
+++ lib/app/presentation/hooks/use_l10n.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+L10n useL10n() {
+  return L10n.of(useContext())!;
+}
--- lib/app/presentation/pages/item/item_page.dart
+++ lib/app/presentation/pages/item/item_page.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:reactive_flutter_rating_bar/reactive_flutter_rating_bar.dart';
 
 import '../../../application/extension/number_extension.dart';
-import '../../../application/state/locale_provider.dart';
 import '../../../application/usecase/item/state/item_detail_providers.dart';
 import '../../../application/usecase/user/state/auth_user_provider.dart';
 import '../../../domain/item/entity/item.dart';
@@ -14,6 +12,8 @@ import '../../../domain/purchase/value_object/purchase_status.dart';
 import '../../../domain/user/entity/user.dart';
 import '../../../domain/user/value_object/age_group.dart';
 import '../../components/importer.dart';
+import '../../components/src/date_text_with_label.dart';
+import '../../hooks/use_l10n.dart';
 import '../../routes/importer.dart';
 import '../error/components/error_view.dart';
 import 'components/item_images.dart';
@@ -24,13 +24,13 @@ class ItemPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
-    final authUser = ref.watch(authUserProvider);
+    final l10n = useL10n();
+    final user = ref.watch(authUserProvider);
     final item = ref.watch(ItemDetailProviders.itemProvider);
     final purchase = ref.watch(ItemDetailProviders.purchaseProvider);
 
     // 欲しい物と購入情報に応じて判定を行う
-    return switch ((item, purchase, authUser)) {
+    return switch ((item, purchase, user)) {
       // 欲しい物が見つからない場合は削除された扱い
       (
         AsyncData(value: null),
@@ -43,12 +43,12 @@ class ItemPage extends HookConsumerWidget {
       (
         AsyncData(value: final itemData),
         AsyncData(value: final purchaseData?),
-        AsyncData(value: final authUser),
+        AsyncData(value: final userData),
       ) =>
         _ItemDetailView(
           item: itemData!,
           purchase: purchaseData,
-          authUser: authUser!,
+          user: userData!,
         ),
 
       // いずれかがエラーの場合はエラー
@@ -63,20 +63,20 @@ class ItemPage extends HookConsumerWidget {
   }
 }
 
-class _ItemDetailView extends HookConsumerWidget {
+class _ItemDetailView extends StatelessWidget {
   const _ItemDetailView({
     required this.item,
     this.purchase,
-    required this.authUser,
+    required this.user,
   });
 
   final Item item;
   final Purchase? purchase;
-  final User authUser;
+  final User user;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
+  Widget build(BuildContext context) {
+    final l10n = useL10n();
 
     return Scaffold(
       appBar: AppBar(
@@ -128,14 +128,17 @@ class _ItemDetailView extends HookConsumerWidget {
               const Gap(40),
               _PurchaseInfo(
                 purchase: purchase,
-                user: authUser,
+                user: user,
               ),
               const Gap(160),
             ],
           ),
         ),
       ),
-      floatingActionButton: const _Fab(),
+      floatingActionButton: _PurchaseFab(
+        item: item,
+        user: user,
+      ),
     );
   }
 
@@ -146,16 +149,16 @@ class _ItemDetailView extends HookConsumerWidget {
 }
 
 /// 購入状況
-class _PurchaseStatus extends HookConsumerWidget {
+class _PurchaseStatus extends StatelessWidget {
   const _PurchaseStatus({required this.purchaseStatus});
 
   final PurchaseStatus purchaseStatus;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
-    final l10n = ref.watch(l10nProvider);
+    final l10n = useL10n();
 
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -172,14 +175,14 @@ class _PurchaseStatus extends HookConsumerWidget {
 }
 
 /// 欲しい度
-class _WishRank extends HookConsumerWidget {
+class _WishRank extends StatelessWidget {
   const _WishRank({this.wishRank});
 
   final double? wishRank;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
+  Widget build(BuildContext context) {
+    final l10n = useL10n();
     final textTheme = Theme.of(context).textTheme;
 
     return Column(
@@ -202,14 +205,14 @@ class _WishRank extends HookConsumerWidget {
 }
 
 /// URL一覧
-class _Urls extends HookConsumerWidget {
+class _Urls extends StatelessWidget {
   const _Urls({this.urls});
 
   final List<String>? urls;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
+  Widget build(BuildContext context) {
+    final l10n = useL10n();
     final textTheme = Theme.of(context).textTheme;
 
     // 未設定の場合はダミー要素だけ表示
@@ -241,7 +244,7 @@ class _Urls extends HookConsumerWidget {
 
 /// 購入情報
 /// ログインユーザーが子供の場合は表示しない
-class _PurchaseInfo extends HookConsumerWidget {
+class _PurchaseInfo extends StatelessWidget {
   const _PurchaseInfo({
     this.purchase,
     required this.user,
@@ -251,40 +254,56 @@ class _PurchaseInfo extends HookConsumerWidget {
   final User user;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // 購入情報は子供には表示しない
+  Widget build(BuildContext context) {
+    final l10n = useL10n();
     final isChild = user.ageGroup == AgeGroup.child;
+
+    // 購入情報は子供には表示しない
     if (isChild) {
       return const SizedBox.shrink();
     }
 
-    // TODO(yakitama5): ここからリファクタリングの続き
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _PurchaseInfoTitle(),
-        Gap(16),
-        _Price(),
-        Gap(16),
-        _PlanDate(),
-        Gap(16),
-        _SentAt(),
-        Gap(16),
-        _BuyerName(),
-        Gap(16),
-        _PurchaseMemo(),
+        const _PurchaseInfoTitle(),
+        const Gap(16),
+        TextWithLabel(
+          purchase?.price?.formatCurrency(locale: l10n.localeName),
+          label: l10n.price,
+        ),
+        const Gap(16),
+        DateTextWithLabel(
+          dateTime: purchase?.planDate,
+          label: l10n.purchasePlanDateTime,
+        ),
+        const Gap(16),
+        DateTextWithLabel(
+          dateTime: purchase?.sentAt,
+          label: l10n.sentAt,
+        ),
+        const Gap(16),
+        TextWithLabel(
+          purchase?.buyerName,
+          label: l10n.buyerName,
+        ),
+        const Gap(16),
+        TextWithLabel(
+          purchase?.memo,
+          label: l10n.memo,
+        ),
       ],
     );
   }
 }
 
 /// 購入情報のタイトル
-class _PurchaseInfoTitle extends HookConsumerWidget {
+class _PurchaseInfoTitle extends StatelessWidget {
   const _PurchaseInfoTitle();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
+  Widget build(BuildContext context) {
+    final l10n = useL10n();
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
@@ -305,131 +324,35 @@ class _PurchaseInfoTitle extends HookConsumerWidget {
   }
 }
 
-/// 金額
-class _Price extends HookConsumerWidget {
-  const _Price();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
-
-    final price = ref.watch(
-      ItemDetailProviders.purchaseProvider.select((snap) => snap.value?.price),
-    );
-
-    return TextWithLabel(
-      price?.formatCurrency(locale: l10n.localeName),
-      label: l10n.price,
-    );
-  }
-}
-
-/// 購入予定日付
-class _PlanDate extends HookConsumerWidget {
-  const _PlanDate();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
-
-    final planDate = ref.watch(
-      ItemDetailProviders.purchaseProvider
-          .select((snap) => snap.value?.planDate),
-    );
-    final formatter = DateFormat.yMMMd(l10n.localeName);
-
-    return TextWithLabel(
-      planDate != null ? formatter.format(planDate) : null,
-      label: l10n.purchasePlanDateTime,
-    );
-  }
-}
-
-/// 渡した日付
-class _SentAt extends HookConsumerWidget {
-  const _SentAt();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
-
-    final sentAt = ref.watch(
-      ItemDetailProviders.purchaseProvider.select((snap) => snap.value?.sentAt),
-    );
-    final formatter = DateFormat.yMMMd(l10n.localeName);
-
-    return TextWithLabel(
-      sentAt != null ? formatter.format(sentAt) : null,
-      label: l10n.sentAt,
-    );
-  }
-}
-
-/// 買った人の名前
-class _BuyerName extends HookConsumerWidget {
-  const _BuyerName();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
-
-    final buyerName = ref.watch(
-      ItemDetailProviders.purchaseProvider
-          .select((snap) => snap.value?.buyerName),
-    );
-
-    return TextWithLabel(
-      buyerName,
-      label: l10n.buyerName,
-    );
-  }
-}
-
-/// 購入情報のメモ
-class _PurchaseMemo extends HookConsumerWidget {
-  const _PurchaseMemo();
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
-    final memo = ref.watch(
-      ItemDetailProviders.purchaseProvider.select((value) => value.value?.memo),
-    );
-
-    return TextWithLabel(
-      memo,
-      label: l10n.memo,
-    );
-  }
-}
-
 /// 購入/購入予定ボタン
+///
 /// ログインユーザーが子供の場合は表示しない
-class _Fab extends HookConsumerWidget {
-  const _Fab();
+class _PurchaseFab extends HookConsumerWidget {
+  const _PurchaseFab({
+    required this.item,
+    required this.user,
+  });
+
+  final Item item;
+  final User user;
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final l10n = ref.watch(l10nProvider);
+    final l10n = useL10n();
+    final isChild = user.ageGroup == AgeGroup.child;
 
     // 子供の場合はボタンを非表示
-    final isChild = ref.watch(
-      authUserProvider
-          .select((value) => value.value?.ageGroup == AgeGroup.child),
-    );
     if (isChild) {
       return const SizedBox.shrink();
     }
 
     return FloatingActionButton.extended(
-      onPressed: () => _onPurchase(context, ref),
+      onPressed: () => _onPurchase(context),
       icon: const Icon(Icons.shopping_bag),
       label: Text(l10n.purchaseOrpurchasePlan),
     );
   }
 
-  void _onPurchase(BuildContext context, WidgetRef ref) {
-    // 画面遷移
-    final itemId = ref.watch(ItemDetailProviders.itemIdProvider);
-    PurchaseRouteData(itemId!).go(context);
-  }
+  void _onPurchase(BuildContext context) =>
+      PurchaseRouteData(item.id).go(context);
 }
