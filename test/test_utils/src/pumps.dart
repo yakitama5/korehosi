3cde9b49bf1a5518b1c9365a23c82c0468aaaba7
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2024-03-18 13:14:45 +0900

    🚨test: 保守されていないテストコードの削除

--- test/test_utils/src/pumps.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-Future<void> pumpRepeatableOneSec(WidgetTester tester, int count) async {
-  for (var i = 0; i < count; i++) {
-    await tester.pump(const Duration(milliseconds: 1000));
-  }
-}
