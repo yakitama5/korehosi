3bdb12004ede5eabf9104f22002add8538147843
yakitama5 <rittitarou@yahoo.co.jp>
2024-08-22 15:14:49 +0900

    ♻️refactor: VerUPに伴うエラー/静的解析結果の対応

--- lib/app/infrastructure/firebase/dynamic_links/state/dynamic_links.dart
+++ lib/app/infrastructure/firebase/dynamic_links/state/dynamic_links.dart
@@ -1,3 +1,6 @@
+// TODO(yakitama5): FirebaseDynamicLinksからの移行
+// ignore_for_file: deprecated_member_use
+
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
