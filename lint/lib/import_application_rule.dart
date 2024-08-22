d7e132315eba5fced2e91379da290436a496295d
yakitama5 <rittitarou@yahoo.co.jp>
2024-08-22 16:48:57 +0900

    🐛fix: Lintパッケージの静的エラー対応

--- lint/lib/import_application_rule.dart
+++ lint/lib/import_application_rule.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:analyzer/error/error.dart';
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
