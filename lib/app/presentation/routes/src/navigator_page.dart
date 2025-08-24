7bcd0c8e3131a905d8b79f8f5ef08cb3c84826bf
yakitama5 <rittitarou@yahoo.co.jp>
2025-08-25 01:06:14 +0900

    ♻️refactor: メンテ中止されたパッケージ`flutter_adaptive_scaffold`の廃止

--- lib/app/presentation/routes/src/navigator_page.dart
+++ lib/app/presentation/routes/src/navigator_page.dart
@@ -1,8 +1,8 @@
-import 'package:custom_adaptive_scaffold/custom_adaptive_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import '../../../application/state/theme_mode_provider.dart';
 import '../../hooks/importer.dart';
@@ -19,86 +19,114 @@ class NavigatorPage extends HookWidget {
   Widget build(BuildContext context) {
     final l10n = useL10n();
 
-    final tabs = [
-      NavigationDestination(
+    final navigationItems = [
+      _NavigationItem(
         icon: const Icon(Icons.shopping_bag_outlined),
         selectedIcon: const Icon(Icons.shopping_bag),
         label: l10n.wishList,
       ),
-      NavigationDestination(
+      _NavigationItem(
         icon: const Icon(Icons.analytics_outlined),
         selectedIcon: const Icon(Icons.analytics),
         label: l10n.analyze,
       ),
-      NavigationDestination(
+      _NavigationItem(
         icon: const Icon(Icons.settings_outlined),
         selectedIcon: const Icon(Icons.settings),
         label: l10n.settings,
       ),
     ];
 
-    final rails = tabs
-        .map(
-          (e) => NavigationRailDestination(
-            label: Text(e.label),
-            // HACK(yakitama5): `Navigation rail`がTooltip非対応
-            /// 対応されたら書き換える
-            /// https://github.com/flutter/flutter/issues/113103
-            icon: Tooltip(
-              message: e.label,
-              child: e.icon,
-            ),
-            selectedIcon: Tooltip(
-              message: e.label,
-              child: e.selectedIcon,
-            ),
-          ),
-        )
-        .toList();
+    final breakpoint = ResponsiveBreakpoints.of(context).breakpoint;
+    return switch (breakpoint.name) {
+      MOBILE => _NavigationBar(
+          navigationShell,
+          navigationItems,
+        ),
+      (_) => _NavigationRail(
+          navigationShell,
+          navigationItems,
+        ),
+    };
+  }
+}
 
-    return AdaptiveLayout(
-      body: SlotLayout(
-        config: <Breakpoint, SlotLayoutConfig>{
-          Breakpoints.standard: SlotLayout.from(
-            key: const Key('Body'),
-            // builder: (_) => BranchSwitcher(
-            //   currentIndex: navigationShell.currentIndex,
-            //   children: children,
-            // ),
-            builder: (_) => navigationShell,
-          ),
-        },
+class _NavigationBar extends HookWidget {
+  const _NavigationBar(this.navigationShell, this.navigationItems);
+
+  final StatefulNavigationShell navigationShell;
+  final List<_NavigationItem> navigationItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: NavigationBar(
+        destinations: navigationItems
+            .map((e) => NavigationDestination(
+                  icon: e.icon,
+                  label: e.label,
+                  selectedIcon: e.selectedIcon,
+                ))
+            .toList(),
+        selectedIndex: navigationShell.currentIndex,
+        onDestinationSelected: _onDestinationSelected,
       ),
-      primaryNavigation: SlotLayout(
-        config: <Breakpoint, SlotLayoutConfig>{
-          Breakpoints.large: SlotLayout.from(
-            key: const Key('Navigation Rails'),
-            inAnimation: AdaptiveScaffold.leftOutIn,
-            outAnimation: AdaptiveScaffold.leftInOut,
-            builder: (_) => AdaptiveScaffold.standardNavigationRail(
-              destinations: rails,
-              selectedIndex: navigationShell.currentIndex,
-              onDestinationSelected: _onDestinationSelected,
-              trailing: const _ThemeModeButton(),
-            ),
+    );
+  }
+
+  void _onDestinationSelected(int index) {
+    navigationShell.goBranch(
+      index,
+      initialLocation: index == navigationShell.currentIndex,
+    );
+  }
+}
+
+class _NavigationRail extends HookWidget {
+  const _NavigationRail(this.navigationShell, this.navigationItems);
+
+  final StatefulNavigationShell navigationShell;
+  final List<_NavigationItem> navigationItems;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: NavigationRail(
+                  destinations: navigationItems
+                      .map((e) => NavigationRailDestination(
+                          label: Text(e.label),
+                          // HACK(yakitama5): `Navigation rail`がTooltip非対応
+                          /// 対応されたら書き換える
+                          /// https://github.com/flutter/flutter/issues/113103
+                          icon: Tooltip(
+                            child: e.icon,
+                            message: e.label,
+                          ),
+                          selectedIcon: Tooltip(
+                            child: e.selectedIcon,
+                            message: e.label,
+                          )))
+                      .toList(),
+                  selectedIndex: navigationShell.currentIndex,
+                  onDestinationSelected: _onDestinationSelected,
+                ),
+              ),
+              const _ThemeModeButton(),
+            ],
           ),
-        },
-      ),
-      bottomNavigation: SlotLayout(
-        config: <Breakpoint, SlotLayoutConfig>{
-          Breakpoints.smallAndUp: SlotLayout.from(
-            key: const Key('Bottom Navigation Small'),
-            inAnimation: AdaptiveScaffold.bottomToTop,
-            outAnimation: AdaptiveScaffold.topToBottom,
-            // builder: (_) => AdaptiveScaffold.standardBottomNavigationBar(
-            builder: (_) => AdaptiveScaffold.standardBottomNavigationBar(
-              destinations: tabs,
-              currentIndex: navigationShell.currentIndex,
-              onDestinationSelected: _onDestinationSelected,
-            ),
+          VerticalDivider(
+            thickness: 1,
+            width: 1,
+            color: Theme.of(context).colorScheme.outline,
           ),
-          Breakpoints.large: SlotLayoutConfig.empty(),
-        },
+          Expanded(child: navigationShell)
+        ],
       ),
     );
   }
@@ -143,4 +171,13 @@ class _ThemeModeButton extends HookConsumerWidget {
       ),
     );
   }
+}
+
+class _NavigationItem {
+  const _NavigationItem(
+      {required this.icon, required this.selectedIcon, required this.label});
+
+  final Icon icon;
+  final Icon selectedIcon;
+  final String label;
 }
