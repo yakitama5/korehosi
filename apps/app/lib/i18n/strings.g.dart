37ffe72f2c9aa66f8aa05daa84e672240628e8d5
yakitama5 <rittitarou@yahoo.co.jp>
2025-10-13 12:39:44 +0900

    🔧chore: GithubActionsをマルチパッケージ構成にあわせて修正

--- apps/app/lib/i18n/strings.g.dart
+++ apps/app/lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 138 (69 per locale)
 ///
-/// Built on 2025-10-12 at 15:10 UTC
+/// Built on 2025-10-13 at 03:36 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
--- packages/designsystem/lib/i18n/strings.g.dart
+++ packages/designsystem/lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 170 (85 per locale)
 ///
-/// Built on 2025-10-12 at 14:08 UTC
+/// Built on 2025-10-13 at 03:36 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint, unused_import
