54d613a700395812a0cf13420ce0c29841c4292e
Yakuran <rittitarou@yahoo.co.jp>
2025-10-23 15:48:29 +0900

    ♻️refactor: `extension type`によるドメイン層の詳細化
     - ID項目の拡張型を定義
     - リポジトリの型を見直し

--- packages/domain/lib/src/common/interface/storage_service.dart
+++ packages/domain/lib/src/common/interface/storage_service.dart
@@ -1,21 +1,22 @@
-import 'dart:typed_data';
-
+import 'package:cross_file/cross_file.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'storage_service.g.dart';
 
+extension type ImageId(String value) {}
+
 /// DI用 (依存性逆転のためドメイン層に定義)
 @Riverpod(keepAlive: true)
 StorageService storageService(Ref ref) =>
-// アプリ起動時 or テスト時に `override` することを前提に利用
+    // アプリ起動時 or テスト時に `override` することを前提に利用
     throw UnimplementedError();
 
 /// ストレージ(ファイル,画像)に関するサービス
 abstract class StorageService {
   /// ダウンロードURLを生成する
-  Future<String> downloadUrl(String path);
+  Future<String> downloadUrl(ImageId id);
 
   /// 画像をアップロードする
-  Future<String> uploadImage(String path, Uint8List uint8List);
+  Future<ImageId> uploadImage(ImageId? id, XFile xfile);
 }
