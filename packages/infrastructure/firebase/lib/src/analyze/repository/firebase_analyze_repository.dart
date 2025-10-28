79ba3a550fad76fe1f93718b6ba49b18db5f79cc
Yakuran <rittitarou@yahoo.co.jp>
2025-10-29 01:25:18 +0900

    ♻️refactor: ドメイン層の詳細化に伴うインフラ層の修正
     - firebase

---
+++ packages/infrastructure/firebase/lib/src/analyze/repository/firebase_analyze_repository.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:infrastructure_firebase/src/item/state/firestore_item_provider.dart';
+import 'package:infrastructure_firebase/src/item/state/firestore_purchase_provider.dart';
+import 'package:packages_domain/analyze.dart';
+import 'package:packages_domain/common.dart';
+import 'package:packages_domain/group.dart';
+import 'package:packages_domain/user.dart';
+
+/// Firebaseを利用したリポジトリの実装
+class FirebaseAnalyzeRepository implements AnalyzeRepository {
+  const FirebaseAnalyzeRepository(this.ref);
+
+  final Ref ref;
+
+  @override
+  Future<ItemBuyedRate> exploreBuyedRate({
+    required GroupId groupId,
+    required AgeGroup ageGroup,
+    required ItemAnalyzeQuery query,
+  }) async {
+    // コレクション定義
+    final itemCol = ref.read(itemCollectionRefProvider(groupId: groupId));
+
+    // 各件数の取得
+    final itemCount = await itemCol.count().get().then((doc) => doc.count);
+    final buyedItemCount =
+        await itemCol
+            .where('sentAt', isNull: false)
+            .count()
+            .get()
+            .then((doc) => doc.count) ??
+        0;
+
+    // 購入率を計算
+    final buyedRate = itemCount == null ? 0.0 : buyedItemCount / itemCount;
+
+    return ItemBuyedRate(
+      buyedItemCount: buyedItemCount,
+      itemCount: itemCount ?? 0,
+      buyedRate: buyedRate,
+    );
+  }
+
+  @override
+  Future<MonthlyTotalsPurchases> exploreMonthlyTotals({
+    required GroupId groupId,
+    required AgeGroup ageGroup,
+    required YearMonth fromYearMonth,
+    required YearMonth toYearMonth,
+    required ItemAnalyzeQuery query,
+  }) async {
+    // コレクション定義
+    final purchaseCol = ref.read(
+      purchaseCollectionRefProvider(groupId: groupId),
+    );
+
+    // 月初めに正規化
+    var currentDate = DateTime(fromYearMonth.year, fromYearMonth.month);
+    final lastDate = DateTime(toYearMonth.year, toYearMonth.month);
+
+    final totals = <MonthlyTotals>[];
+    while (currentDate.isBefore(lastDate) ||
+        currentDate.isAtSameMomentAs(lastDate)) {
+      // 単月の結果を取得
+      final currentTimestamp = Timestamp.fromDate(currentDate);
+      final price = await purchaseCol
+          .where('sentAt', isGreaterThanOrEqualTo: currentTimestamp)
+          .aggregate(sum('price'))
+          .get()
+          .then((res) => res.getSum('price'));
+
+      // 結果を格納
+      totals.add(
+        MonthlyTotals(
+          yearMonth: YearMonth(
+            year: currentDate.year,
+            month: currentDate.month,
+          ),
+          totalPrice: (price ?? 0.0).toInt(),
+        ),
+      );
+
+      // 次の月へ進める
+      currentDate = DateTime(currentDate.year, currentDate.month + 1);
+    }
+
+    return MonthlyTotalsPurchases(monthlyTotals: totals);
+  }
+}
