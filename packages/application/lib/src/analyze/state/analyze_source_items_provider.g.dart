e2b8c1965136b9c8ab77f4226f723db26cb95ac2
Yakuran <rittitarou@yahoo.co.jp>
2025-10-28 23:19:22 +0900

    ♻️generete: regenerate code

--- packages/application/lib/src/analyze/state/analyze_source_items_provider.g.dart
+++ packages/application/lib/src/analyze/state/analyze_source_items_provider.g.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: type=lint, duplicate_ignore
+
 part of 'analyze_source_items_provider.dart';
 
 // **************************************************************************
