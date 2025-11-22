bb47889ec8025a7d2531c0d5f33b24f69bc3e7da
Yakuran <rittitarou@yahoo.co.jp>
2025-11-22 20:42:34 +0900

    🚨test: CI時の差許容率を縮小

--- apps/app/test/test_utils/src/prepare_golden_file_comparator_with_threshold.dart
+++ apps/app/test/test_utils/src/prepare_golden_file_comparator_with_threshold.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'local_file_comparator_with_threshold.dart';
 
 void prepareGoldenFileComparatorWithThreshold({
-  double threshold = 0.03, // 3%
+  double threshold = 0.01, // 1%
 }) {
   if (goldenFileComparator is LocalFileComparator) {
     final testUrl = (goldenFileComparator as LocalFileComparator).basedir;
